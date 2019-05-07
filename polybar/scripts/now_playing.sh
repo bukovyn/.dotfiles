8b7a77b8795fffc3afffa21eed6256084baf930c
bukovyn <bukovyn@gmail.com>
2019-05-07 10:16:59 -0400

    update configs, add polybar scripts

---
+++ polybar/scripts/now_playing.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+STATUS=$(playerctl status 2> /dev/null)
+
+if [ "$STATUS" = "Playing" ]; then
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+elif [ "$STATUS" = "Paused" ]; then
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+else
+    echo ""
+fi
