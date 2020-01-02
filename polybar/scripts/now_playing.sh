be9c72ce271c8a006adecda580d96ce196c61298
bukovyn <bukovyn@gmail.com>
2020-01-01 23:43:19 -0500

    Update dotfiles

--- polybar/scripts/now_playing.sh
+++ polybar/scripts/now_playing.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-STATUS=$(playerctl status 2> /dev/null)
+STATUS=$(playerctl -p spotify status 2> /dev/null)
 
 if [ "$STATUS" = "Playing" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
 elif [ "$STATUS" = "Paused" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
 else
     echo ""
 fi
